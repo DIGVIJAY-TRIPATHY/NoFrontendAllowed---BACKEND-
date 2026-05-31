9445243e2cb034cb7e9d99811d36bb26800eedcc
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-06-01 00:04:41 +0530

    refreshAccessToken endpoint added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,8 @@ import {Router} from 'express';
 import {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 } from '../controllers/user.controller.js';
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +30,6 @@ router.route("/login").post(loginUser);
 
 //secured route
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
