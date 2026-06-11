a203e583e55db15373613cda8dbbd6eeec3afcf4
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-06-11 11:27:41 +0530

    refactor subscription routes and controllers for improved functionality and error handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,4 +1,4 @@
-import mongoose, {isValidObjectId} from "mongoose"
+import mongoose, {isValidObjectId, Mongoose} from "mongoose"
 import {User} from "../models/user.model.js"
 import { Subscription } from "../models/subscription.model.js"
 import {ApiError} from "../utils/ApiError.js"
@@ -52,11 +52,101 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid Channel Id")
+    }
+
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscribers",
+                foreignField: "_id",
+                as: "subscriber"
+            }
+        },
+        {
+            $unwind: "$subscriber"
+        },
+        {
+            $project: {
+                _id: 0,
+                subscriberId: "$subscriber._id",
+                username: "$subscriber.username",
+                fullName: "$subscriber.fullName",
+                avatar: "$subscriber.avatar"
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                totalSubscribers: subscribers.length,
+                subscribers
+            },
+            "Subscribers fetched successfully"
+        )
+    )
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if(!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriber Id")
+    }
+
+    const subscribedChannels = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channel"
+            }
+        },
+        {
+            $unwind: "$channel"
+        },
+        {
+            $project: {
+                _id: 0,
+                channelId: "$channel._id",
+                username: "$channel.username",
+                fullName: "$channel.fullName",
+                avatar: "$channel.avatar"
+            }
+        }
+    ]);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                totalSubscribedChannels: subscribedChannels.length,
+                subscribedChannels
+            },
+            "Subscribed channels fetched successfully"
+        )
+    );
 })
 
 export {
