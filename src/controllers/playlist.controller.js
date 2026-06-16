2fccaf865c8315a0036700a8a65670de64ec24bc
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-06-16 23:13:34 +0530

    add validation and error handling for playlist creation and user playlist retrieval

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -6,14 +6,85 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
+    //TODO: create playlist
+
     const {name, description} = req.body
 
-    //TODO: create playlist
+    if (!name || !description) {
+        throw new ApiError(400, "Name and description fields are required")
+    }
+
+    const playlist = await Playlist.create({
+        name,
+        description,
+        owner: req.user?._id
+    })
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            201,
+            playlist,
+            "Playlist created successfully"
+        )
+    )
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
     //TODO: get user playlists
+
+    const {userId} = req.params
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid userId")
+    }
+
+    const playlists = await Playlist.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "videos"
+            }
+        },
+        {
+            $addFields: {
+                totalVideos: {
+                    $size: "$videos"
+                },
+                totalViews: {
+                    $sum: "$videos.views"
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                name: 1,
+                description: 1,
+                totalVideos: 1,
+                totalViews: 1,
+                updatedAt: 1
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            playlists,
+            "User playlists fetched successfully"
+        )
+    )
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
